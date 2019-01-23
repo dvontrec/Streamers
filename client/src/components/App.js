14bce8d8f50a9d24470ca11a073f99a69ead80bb
Dvontre Coleman <dvontrec@gmail.com>
2019-01-22 19:45:45 -0500

    Wired up header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="ui container">
         <BrowserRouter>
           <div>
+            <Header />
             <Route path="/" exact component={StreamList} />
             <Route path="/streams/new" component={StreamCreate} />
             <Route path="/streams/edit" component={StreamEdit} />
