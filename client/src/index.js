23b2424dcfacb4b0947ca8fbd3c60309ad72bea2
Dvontre Coleman <dvontrec@gmail.com>
2019-01-29 16:49:23 -0500

    Add redux debug tool config

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 import './style.css';
 
-const createStroeWithMiddleware = applyMiddleware()(createStore);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const createStroeWithMiddleware = composeEnhancers(applyMiddleware())(
+  createStore
+);
 // Renders the App element in the document element with the root ID
 ReactDom.render(
   // wraps the app in the provider to allow all components to acces the store
