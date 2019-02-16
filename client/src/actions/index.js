51ee0350b845d0be6c692e71a8455fedab3cea85
Dvontre Coleman <dvontrec@gmail.com>
2019-02-16 18:08:39 -0500

    Add update stream functionality

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -35,8 +35,10 @@ export const fetchStream = id => async dispatch => {
   dispatch({ type: 'FETCH_STREAM', payload: response.data });
 };
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  // patch instead of put to update SOME values
+  const response = await streams.patch(`/streams/${id}`, formValues);
   dispatch({ type: 'EDIT_STREAM', payload: response.data });
+  history.push('/');
 };
 export const deleteStream = id => async dispatch => {
   await streams.delete(`/streams/${id}`);
